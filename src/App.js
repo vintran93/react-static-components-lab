356ceeb07f684df55f8ef8e805d0c8e6646eaa68
Liz Burton <liz.burton147@gmail.com>
2021-09-16 10:39:46 -0400

    Remove named import to fix sequencing issue

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import CatComponent from './CatComponent.js'
-import {GraceHopperQuoteComponent} from './GraceHopperQuoteComponent.js'
+import GraceHopperQuoteComponent from './GraceHopperQuoteComponent.js'
 import MouseComponent from './MouseComponent.js'
 
 class App extends Component {
